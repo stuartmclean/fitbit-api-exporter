8fbc3f72763b0ac231aa9c1780d526ba6d1ec1fe
Fabio Scaccabarozzi <fsvm88@gmail.com>
2020-02-28 00:01:56 +0100

    Start implementing data fetching from APIs and writing to DB

--- api_poller.py
+++ api_poller.py
@@ -2,6 +2,20 @@
 
 import argparse
 import os
+import time
+
+from fitbit import Fitbit
+from influxdb import InfluxDBClient
+
+def create_api_datapoint(measurement, time, fields, tag="data_dump"):
+    return {
+            "measurement": measurement,
+            "tags": {
+                "imported_from": "API"
+                },
+            "time": time,
+            "fields": fields
+            }
 
 def run_api_poller():
     os_env = os.environ
@@ -9,9 +23,27 @@ def run_api_poller():
     client_id = os_env.get('CLIENT_ID', '')
     client_secret = os_env.get('CLIENT_SECRET', '')
     access_token = os_env.get('ACCESS_TOKEN', '')
+    redirect_url = os_env.get('CALLBACK_URL', '')
+    code = os_env.get('CODE', '')
+    units = os_env.get('UNITS', '')
 
     if (not client_id and not client_secret) or not access_token:
         raise Exception('No auth info given!')
+
+    client = Fitbit(client_id, client_secret, redirect_uri=redirect_url, system=units)
+    while True:
+        # Get an influxDB client connection
+        client = InfluxDBClient('localhost', 8086, 'root', 'root', 'fitbit')
+        client.create_database('fitbit')
+
+        for point in client.body():
+            point['dt'] = point['date'] + ' ' + point['time']
+            client.write_points([create_datapoint('fat', point['dt'], float(point['fat']))])
+
+        for point in client.activities():
+
+
+        time.sleep(60*60)
 
 
 if __name__ == "__main__":
